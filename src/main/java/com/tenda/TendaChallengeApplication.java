3c067a9c55ba6f7a4e22ffac9dd67ea8f36e3b19
Eric Ferreira <ferreira14eric@gmail.com>
2025-12-09 13:44:01 -0300

    Feature: First Commit

---
+++ src/main/java/com/tenda/TendaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TendaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenda/TendaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
